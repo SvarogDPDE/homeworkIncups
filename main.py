c4ed00f2a64f9be1e3fc59ff53c38963e7b7d388
Svarog <SvarogDPDE@yandex.ru>
2023-11-01 00:31:05 +0300

    inheritance

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ class Mentor:
         else:
             return 'Ошибка'
 
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        return False
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        return False
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
